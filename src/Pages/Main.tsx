ffd2ae24dd454d977be1357fad7c1cd27fb0e3f0
Richard Montoya <richard.a.montoya.l@gmail.com>
2023-08-26 16:15:50 -0500

    run prettier

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function Main() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      some random tesxt
-    </>
-  )
+  return <>some random tesxt</>;
 }
 
-export default Main
+export default Main;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Main from './Pages/Main.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Main from "./Pages/Main.tsx";
+import "./index.css";
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
-      <Route path="/" element={<Main />}/>
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   </React.StrictMode>,
-)
+);
